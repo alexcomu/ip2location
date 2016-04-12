bb2a36f7de766085d9a3e2718554c5912ea118a0
alexcomu <alex.comunian@gmail.com>
2016-04-12 14:52:48 +0200

    save all

--- ipmap.py
+++ ipmap.py
@@ -14,7 +14,13 @@ def process(inputfile):
                     "range1":int(row[0]),
                     "range2":int(row[1]),
                     "country_code":row[2],
-                    "country":row[3]
+                    "country":row[3],
+                    "region":row[4],
+                    "city" = row[5],
+                    "lat" = row[6],
+                    "lng" = row[7],
+                    "zipcode" = row[8],
+                    "timezone" = row[9]
                 })
             print "Insert: ", data
 
